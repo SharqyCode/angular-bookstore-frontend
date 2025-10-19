c79bea4814e751b881ab41c4181cfb9f44003d32
Ebraheem <pencilmagicart@gmail.com>
2025-10-19 22:59:13 +0300

    navigation through redirection only

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+  imports: [RouterLink],
 })
 export class NavbarComponent {
   isLoggedIn = false;
@@ -17,7 +18,7 @@ export class NavbarComponent {
   }
 
   logout() {
-    this.authService.logout();
+    // this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
